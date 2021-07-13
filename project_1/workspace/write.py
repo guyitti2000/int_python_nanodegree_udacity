bb9fcbcb1a09ae31f34f38f87e221bf80d0edc76
Riley Ittidecharchoti <polyglotpengyou@gmail.com>
2021-07-13 02:52:33 -0700

    write.py updated

--- project_1/workspace/write.py
+++ project_1/workspace/write.py
@@ -24,13 +24,14 @@ def write_to_csv(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    fieldnames = ('datetime_utc'='', 'distance_au'='', 'velocity_km_s'='', 'designation'='', 'name'='', 'diameter_km'='', 'potentially_hazardous'=False)
+    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
     # TODO: Write the results to a CSV file, following the specification in the instructions.
     with open('close_approach.csv', 'w') as outfile:
         writer = csv.DictWriter(outfile, fieldnames)
         writer.writeheader()
-        for elem in results:
-            writer.write_results(elem)
+        for approach in results:
+            row = dict([('datetime_utc', ''), ('distance_au', ''), ('velocity_km_s', ''), ('designation', ''), ('name', ''),  ('diameter_km', ''),  ('potentially_hazardous', '')])
+            writer.writerow(row)
 
 
 def write_to_json(results, filename):
@@ -45,5 +46,9 @@ def write_to_json(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # TODO: Write the results to a JSON file, following the specification in the instructions.
+    dict_list = []
+    for approach in results:
+        ca_dict = {'datetime_utc': approach.time_str, 'distance_au': approach.distance, 'velocity_km_s': approach.velocity, 'neo': {'designation': approach.neo.designation, 'name': approach.neo.name, 'diameter_km': approach.neo.diameter, 'potentially_hazardous': approach.neo.hazardous}}
+        dict_list.append(ca_dict)
     with open('close_approach.json', 'w') as outfile:
-        json.dump(results, outfile, indent =2)
+        json.dump(dict_list, outfile, indent =2)
