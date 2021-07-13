8dd5ae3df21c0d70ef2568f89d5ee302af3136af
Riley Ittidecharchoti <polyglotpengyou@gmail.com>
2021-07-13 00:31:19 -0700

    stuck on task 4

--- project_1/workspace/write.py
+++ project_1/workspace/write.py
@@ -26,6 +26,11 @@ def write_to_csv(results, filename):
     """
     fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
     # TODO: Write the results to a CSV file, following the specification in the instructions.
+    with open('close_approach.csv', 'w') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for elem in results:
+            writer.writerow(elem)
 
 
 def write_to_json(results, filename):
@@ -40,3 +45,5 @@ def write_to_json(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # TODO: Write the results to a JSON file, following the specification in the instructions.
+    with open('close_approach.json', 'w') as outfile:
+        json.dump(results, outfile, indent=2)
